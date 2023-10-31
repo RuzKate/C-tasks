23e6badd051b6752979b16232eaf70f4e81de5fd
Екатерина <kateryzhevske16@gmail.com>
2023-10-31 21:43:03 +0300

    Create array(dop).cs
    dop

---
+++ array(dop).cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int[] answer = new int[nums.Length];
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            answer[i] = Math.Abs(nums.Take(i).Sum() - nums.Skip(i + 1).Sum());
+        }
+        Console.WriteLine(string.Join(" ", answer));
+    }
+}
